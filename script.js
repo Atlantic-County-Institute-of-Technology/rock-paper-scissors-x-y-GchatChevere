2572da2f402cfed6a838c0f3b51724ce36156153
GchatChevere <chev0050@student.acitech.org>
2025-03-21 09:13:38 -0400

    i started js im now confused(im gonna work at home on it)

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-const btn = document.querySelector("")
+const Rock = document.getElementById("Rock")
+const Paper = document.getElementById("paper")
+const Scissors = document.getElementById("scissors")
+const Microwave = document.getElementById("microwave")
+const Pencil = document.getElementById("pencil")
+
+
+
+let playerPick = 0
+let cpuPick = 0
+
+// TODO: EventListeners for player choice
+// TODO: Function for cpu choice
+
+
+button.addEventListener("click", function(Rock));
+
+function setPlayerChoice(choice); {
+    playerPick = choice
+}
+if(choice == Rock){
+    alert("Nice");
+}
+
+
+
+
+// function checkScore(){
+//     if (playerPick == 1){
+//         alert("you did it")   
+//     alert("You Did it!")
+// }
+
+
+
+// }
+
