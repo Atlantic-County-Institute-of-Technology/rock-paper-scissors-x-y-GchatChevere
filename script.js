1c5baf30ddf1ab654cdba14e7868f883733fe4d6
GchatChevere <chev0050@student.acitech.org>
2025-03-24 09:18:25 -0400

    javascript is left

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-const Rock = document.getElementById("Rock")
-const Paper = document.getElementById("paper")
-const Scissors = document.getElementById("scissors")
-const Microwave = document.getElementById("microwave")
-const Pencil = document.getElementById("pencil")
+const Rock = document.getElementById("Rock");
+const Paper = document.getElementById("paper");
+const Scissors = document.getElementById("scissors");
+const Microwave = document.getElementById("microwave");
+const Pencil = document.getElementById("pencil");
+const computerdisplay = document.getElementById("P1");
+const playerDisplay = document.getElementById("P2");
+const result = document.getElementById("winner");
+let playerPick = 0;
+let cpuPick = 0;
 
-
-
-let playerPick = 0
-let cpuPick = 0
 
 // TODO: EventListeners for player choice
 // TODO: Function for cpu choice
 
 
-button.addEventListener("click", function(Rock));
+Rock.addEventListener("click", () => {
+    Playgame(1);
+});
+Paper.addEventListener("click", () => {
+    Playgame(2);
+});
+Scissors.addEventListener("click", () => {
+    Playgame(3);
+});
+Microwave.addEventListener("click", () => {
+    Playgame(4);
+});
+Pencil.addEventListener("click", () => {
+    Playgame(5);
+});
 
-function setPlayerChoice(choice); {
-    playerPick = choice
+function Playgame(playerPick) {
+    
 }
-if(choice == Rock){
-    alert("Nice");
+
+
+
+function cpuChoice() {
+    return Math.floor(Math.random() * 5)
 }
+
 
 
 
